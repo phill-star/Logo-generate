2e30d8a56db2cb6d2f8270827ca5259153eea83f
Mohamed Hussein <mohamedhussein@Mohameds-MacBook-Pro.local>
2023-06-24 08:19:22 -0600

    ChangedFromAnIFToSwitch

--- index.js
+++ index.js
@@ -3,24 +3,28 @@ const fs = require('fs');
 function generateSVG(text, textColor, shape, shapeColor) {
   let svgContent;
 
-  if (shape === 'circle') {
-    svgContent = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-      <circle cx="150" cy="100" r="80" fill="${shapeColor}" />
-      <text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>
-    </svg>`;
-  } else if (shape === 'square') {
-    svgContent = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-      <rect x="70" y="40" width="160" height="160" fill="${shapeColor}" />
-      <text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>
-    </svg>`;
-  } else if (shape === 'triangle') {
-    svgContent = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-      <polygon points="150,20 20,180 280,180" fill="${shapeColor}" />
-      <text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>
-    </svg>`;
-  } else {
-    console.error('Invalid shape:', shape);
-    return;
+  switch (shape) {
+    case 'circle':
+      svgContent = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="150" cy="100" r="80" fill="${shapeColor}" />
+        <text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>
+      </svg>`;
+      break;
+    case 'square':
+      svgContent = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+        <rect x="70" y="40" width="160" height="160" fill="${shapeColor}" />
+        <text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>
+      </svg>`;
+      break;
+    case 'triangle':
+      svgContent = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+        <polygon points="150,20 20,180 280,180" fill="${shapeColor}" />
+        <text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>
+      </svg>`;
+      break;
+    default:
+      console.error('Invalid shape:', shape);
+      return;
   }
 
   fs.writeFile('logo.svg', svgContent, (err) => {
